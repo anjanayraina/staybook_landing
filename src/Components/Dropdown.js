e17a40213caf28be2d8ca340143c0816bac8c0a6
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-01 17:01:38 +0530

    Some UI/UX Changes

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,11 +1,22 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 // import styled from "styled-components";
-
+import '../styles/Dropdown.css';
 function Dropdown(){
 
 return(
+<nav role="navigation">
+  <ul>
+    <li><a href="#" aria-haspopup="true">All Hotels</a>
+      <ul class="dropdown" aria-label="submenu">
+        <li><a href="#">Hotel Aira Xing</a></li>
+        <li><a href="#">Sub-2</a></li>
+        <li><a href="#">Sub-3</a></li>
+      </ul>
+    </li>
+  
+  </ul>
+</nav>
 
-<div className = "DropDown" >All Hotels</div>
 
 )
 
