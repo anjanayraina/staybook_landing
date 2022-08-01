bd03cfa4df40fce51819b639b1958e62cbfb510a
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-01 17:26:42 +0530

    Navbar All Done

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -8,9 +8,13 @@ return(
   <ul>
     <li><a href="#" aria-haspopup="true">All Hotels</a>
       <ul class="dropdown" aria-label="submenu">
-        <li><a href="#">Hotel Aira Xing</a></li>
-        <li><a href="#">Sub-2</a></li>
-        <li><a href="#">Sub-3</a></li>
+        <li className = "hotels"><a href="#" >Hotel Aira Xing</a></li>
+        <li className = "hotels"><a href="#" >Hotel Jyoti Mahal</a></li>
+        <li className = "hotels"><a href="#" >Hotel Pinky Villa</a></li>
+        <li className = "hotels"><a href="#" >Hotel Jai Balaji</a></li>
+        <li  className = "hotels"><a href="#">Staybook South Delhi</a></li>
+        <li className = "hotels"><a href="#" >Shiv Dev International</a></li>
+        <li className = "hotels"><a href="#" >Staybook Woods View </a></li>
       </ul>
     </li>
   
--- src/Components/ImageAndBody.js
+++ src/Components/ImageAndBody.js
@@ -3,9 +3,9 @@ import image from "../images/hotelimage.jpg";
 import '../styles/ImageAndBody.css'
  function ImageAndBody() {
   return (
-    <div class="container">
+    <div className ="container">
     <img src={image} alt="Logo" className = "HotelImage"/>
-    <div class="centered">Budget Hotels at Every Corner of India</div>
+    <div className ="centered">Budget Hotels at Every Corner of India</div>
    
   </div>
   )
